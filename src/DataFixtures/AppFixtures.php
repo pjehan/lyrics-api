d156193bf8f692221ffc45fc69314fb14b5501b6
pjehan <pierre.jehan@gmail.com>
2023-02-18 16:14:16 +0100

    Generate Music fixtures from CSV file

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\ApiResource\Music;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,27 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        // Loop through CSV file and save each row as a new Music entity
+        $file = new \SplFileObject(__DIR__ . '/data.csv');
+        $file->setFlags(\SplFileObject::READ_CSV | \SplFileObject::SKIP_EMPTY | \SplFileObject::DROP_NEW_LINE);
+        $row = 0;
+        foreach ($file as $line) {
+            if ($row > 0 && is_array($line)) {
+                $music = new Music();
+                $music->setTitle($line[0]);
+                $music->setArtist($line[2]);
+                $year = new \DateTimeImmutable($line[1]);
+                $music->setYear($year->format('Y'));
+                $music->setGenres($line[3]);
+                $music->setLyrics($line[4]);
+                $manager->persist($music);
+                if ($row % 100 === 0) {
+                    $manager->flush();
+                    echo "Saved $row rows" . PHP_EOL;
+                }
+            }
+            $row++;
+        }
         $manager->flush();
     }
 }
